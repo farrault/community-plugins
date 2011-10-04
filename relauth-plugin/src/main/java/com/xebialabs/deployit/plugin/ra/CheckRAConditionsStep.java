0bdbfe218138b36ee0ebfa095058fc5e21170a2f
Martin van Vliet <mvanvliet@xebia.com>
2011-10-04 15:44:15 +0200

    Conditions are now checked when plan is generated as well as when it is executed (audit purposes). Finished short document.

--- relauth-plugin/src/main/java/com/xebialabs/deployit/plugin/ra/CheckRAConditionsStep.java
+++ relauth-plugin/src/main/java/com/xebialabs/deployit/plugin/ra/CheckRAConditionsStep.java
@@ -1,14 +1,10 @@
 package com.xebialabs.deployit.plugin.ra;
 
-import java.util.Collection;
-import java.util.Set;
-
-import org.apache.commons.lang.StringUtils;
-
 import com.xebialabs.deployit.plugin.api.deployment.execution.DeploymentExecutionContext;
 import com.xebialabs.deployit.plugin.api.deployment.execution.DeploymentStep;
 import com.xebialabs.deployit.plugin.api.udm.Environment;
 import com.xebialabs.deployit.plugin.api.udm.Version;
+import com.xebialabs.deployit.plugin.ra.RAConditionVerifier.VerificationResult;
 
 /**
  * Step that verifies that conditions listed in the environment's 'conditions' synthetic field (type: Set<String>)
@@ -19,6 +15,7 @@ import com.xebialabs.deployit.plugin.api.udm.Version;
  * - String: the property is non-blank
  * - Collection properties: the collection is non-empty.
  */
+@SuppressWarnings("serial")
 public class CheckRAConditionsStep implements DeploymentStep {
 
 	private final Environment environment;
@@ -34,43 +31,18 @@ public class CheckRAConditionsStep implements DeploymentStep {
 		return "Verify release authorization for deployment to " + environment.getName();
 	}
 
-	@SuppressWarnings("rawtypes")
 	@Override
 	public com.xebialabs.deployit.plugin.api.execution.Step.Result execute(DeploymentExecutionContext ctx) throws Exception {
-		Set<String> conditions = environment.getSyntheticProperty("conditions");
+		RAConditionVerifier verifier = new RAConditionVerifier(version, environment);
+		VerificationResult result = verifier.verify();
 		
-		for (String condition : conditions) {
-			Object conditionProperty = version.getSyntheticProperty(condition);
-
-			// Handles non-existent property as well as null object properties
-			if (conditionProperty == null) {
-				logFailedValidation(ctx, condition);
-				return Result.Fail;
-			}
-			
-			if (conditionProperty instanceof String && StringUtils.isBlank((String) conditionProperty)) {
-				logFailedValidation(ctx, condition);
-				return Result.Fail;				
-			}
-			
-			if (conditionProperty instanceof Boolean && ! (Boolean) conditionProperty) {
-				logFailedValidation(ctx, condition);
-				return Result.Fail;				
-			}
-
-			if (conditionProperty instanceof Collection && ((Collection) conditionProperty).isEmpty()) {
-				logFailedValidation(ctx, condition);
-				return Result.Fail;				
-			}
-
-			ctx.logOutput("Release condition '" + condition + "': OK");
+		if (result.isSuccess()) {
+			ctx.logOutput(result.getLog());
+			return Result.Success;
+		} else {
+			ctx.logError(result.getLog());
+			return Result.Fail;
 		}
-		
-		return Result.Success;
-	}
-
-	private void logFailedValidation(DeploymentExecutionContext ctx, String condition) {
-		ctx.logError("Package " + version + " failed to meet release condition '" + condition + "'");
 	}
 
 	@Override
