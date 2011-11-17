27745c883948834fb072497aa7b1c7d120d6a63b
Andrew Phillips <demobox1@yahoo.co.uk>
2011-11-17 16:16:13 +0100

    Initial commit of https://github.com/demobox/release-auth-plugin to the community repo

---
+++ release-auth-plugin/src/main/java/com/xebialabs/deployit/plugins/releaseauth/planning/CheckReleaseConditionsAreMet.java
@@ -0,0 +1,79 @@
+/*
+ * @(#)CheckRequiredChangeRequest.java     26 Aug 2011
+ *
+ * Copyright © 2010 Andrew Phillips.
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ====================================================================
+ */
+package com.xebialabs.deployit.plugins.releaseauth.planning;
+
+import static com.google.common.collect.Sets.newHashSet;
+import static java.lang.Boolean.TRUE;
+
+import java.util.List;
+import java.util.Set;
+
+import com.google.common.collect.ImmutableList;
+import com.xebialabs.deployit.plugin.api.deployment.execution.DeploymentStep;
+import com.xebialabs.deployit.plugin.api.deployment.planning.PostPlanProcessor;
+import com.xebialabs.deployit.plugin.api.deployment.specification.DeltaSpecification;
+import com.xebialabs.deployit.plugin.api.udm.DeployedApplication;
+
+public class CheckReleaseConditionsAreMet {
+    public static final String ENV_RELEASE_CONDITIONS_PROPERTY = "releaseConditions";
+    private static final List<DeploymentStep> NO_STEPS = ImmutableList.of();
+    
+    // allow other plugins to dynamically set/modify release conditions before validation
+    @PostPlanProcessor
+    public static List<DeploymentStep> validate(DeltaSpecification spec) {
+        Set<String> violatedConditions = validateReleaseConditions(spec.getDeployedApplication());
+        if (!violatedConditions.isEmpty()) {
+            throw new IllegalArgumentException(buildErrorMessage(
+                    spec.getDeployedApplication(), violatedConditions));
+        }
+        return NO_STEPS;
+    }
+    
+    protected static Set<String> validateReleaseConditions(DeployedApplication deployedApplication) {
+        Set<String> conditions = deployedApplication.getEnvironment().getSyntheticProperty(ENV_RELEASE_CONDITIONS_PROPERTY);
+        if ((conditions == null) || (conditions.isEmpty())) {
+            return newHashSet();
+        }
+        
+        Set<String> violatedConditions = newHashSet();
+        for (String conditionName : conditions) {
+            if (!TRUE.equals(deployedApplication.getVersion()
+                             .getSyntheticProperty(conditionName))) {
+                violatedConditions.add(conditionName);
+            }
+        }
+        return violatedConditions;
+    }
+    
+    private static String buildErrorMessage(DeployedApplication deployedApplication,
+            Set<String> violatedConditions) {
+        StringBuilder errorMessage = new StringBuilder();
+        errorMessage.append("Cannot deploy '").append(deployedApplication.getName())
+        .append("' (version ").append(deployedApplication.getVersion().getVersion())
+        .append(") to '").append(deployedApplication.getEnvironment().getName())
+        .append("' as the following release conditions are not met:");
+        for (String violatedConditionName : violatedConditions) {
+            errorMessage.append("\n- '").append(violatedConditionName).append("'");
+        }
+        return errorMessage.toString();
+    }
+
+}
---
+++ release-auth-plugin/src/test/java/com/xebialabs/deployit/plugins/releaseauth/planning/CheckReleaseConditionsAreMetTest.java
@@ -0,0 +1,94 @@
+/*
+ * @(#)CheckReleaseConditionsAreMetTest.java     26 Sep 2011
+ *
+ * Copyright © 2010 Andrew Phillips.
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ====================================================================
+ */
+package com.xebialabs.deployit.plugins.releaseauth.planning;
+
+import static com.google.common.collect.Sets.newHashSet;
+import static com.xebialabs.deployit.deployment.planner.DeltaSpecificationBuilder.newSpecification;
+import static com.xebialabs.deployit.test.support.TestUtils.createDeploymentPackage;
+import static com.xebialabs.deployit.test.support.TestUtils.createEnvironment;
+import static com.xebialabs.deployit.test.support.TestUtils.newInstance;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.xebialabs.deployit.deployment.planner.DeltaSpecificationBuilder;
+import com.xebialabs.deployit.plugin.api.boot.PluginBooter;
+import com.xebialabs.deployit.plugin.api.deployment.specification.DeltaSpecification;
+import com.xebialabs.deployit.plugin.api.udm.Deployed;
+import com.xebialabs.deployit.plugin.api.udm.Environment;
+import com.xebialabs.deployit.plugin.test.yak.ci.YakServer;
+
+/**
+ * Unit tests for {@link CheckReleaseConditionsAreMet}
+ */
+public class CheckReleaseConditionsAreMetTest {
+    
+    @BeforeClass
+    public static void boot() {
+        PluginBooter.bootWithoutGlobalContext();
+    }
+    
+    @Test
+    public void ignoresEnvironmentsWithoutReleaseConditions() {
+        CheckReleaseConditionsAreMet.validate(newDeltaSpec(newEnvironment()).build());
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void failsIfAReleaseConditionsIsNull() {
+        Environment env = newEnvironment();
+        env.putSyntheticProperty("releaseConditions", newHashSet("hasReleaseNotes"));
+        CheckReleaseConditionsAreMet.validate(newDeltaSpec(env).build());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void failsIfAReleaseConditionsIsFalse() {
+        Environment env = newEnvironment();
+        env.putSyntheticProperty("releaseConditions", newHashSet("hasReleaseNotes"));
+        DeltaSpecification deltaSpec = newDeltaSpec(env).build();
+        deltaSpec.getDeployedApplication().getVersion().putSyntheticProperty("hasReleaseNotes", FALSE);
+        CheckReleaseConditionsAreMet.validate(deltaSpec);
+    }
+    
+    @Test
+    public void succeedsIfReleaseConditionsAreMet() {
+        Environment env = newEnvironment();
+        env.putSyntheticProperty("releaseConditions", newHashSet("hasReleaseNotes"));
+        DeltaSpecification deltaSpec = newDeltaSpec(env).build();
+        deltaSpec.getDeployedApplication().getVersion().putSyntheticProperty("hasReleaseNotes", TRUE);
+        CheckReleaseConditionsAreMet.validate(deltaSpec);
+    }
+    
+    private static Environment newEnvironment() {
+        return createEnvironment((YakServer) newInstance(YakServer.class));
+    }
+    
+    private static DeltaSpecificationBuilder newDeltaSpec(Environment env,
+            Deployed<?, ?>... newDeployeds) {
+        DeltaSpecificationBuilder deltaSpec = 
+            newSpecification().initial(createDeploymentPackage(), env);
+        for (Deployed<?, ?> newDeployed : newDeployeds) {
+            deltaSpec.create(newDeployed);
+        }
+        return deltaSpec;
+    }
+}
