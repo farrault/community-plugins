060923d4321a7e522f0dd1335ffcd4a34a084133
Andrew Phillips <demobox1@yahoo.co.uk>
2011-12-21 00:24:24 -0500

    Made MailServer an 'Infrastructure' rooted container, basically a cut-down Host

--- notifications-plugin/src/main/java/com/xebialabs/deployit/plugins/notifications/email/ci/MailServer.java
+++ notifications-plugin/src/main/java/com/xebialabs/deployit/plugins/notifications/email/ci/MailServer.java
@@ -20,23 +20,25 @@
  */
 package com.xebialabs.deployit.plugins.notifications.email.ci;
 
+import static com.xebialabs.deployit.plugin.api.udm.Metadata.ConfigurationItemRoot.INFRASTRUCTURE;
+
 import org.codemonkey.simplejavamail.Mailer;
 import org.codemonkey.simplejavamail.TransportStrategy;
 
 import com.xebialabs.deployit.plugin.api.udm.Metadata;
-import com.xebialabs.deployit.plugin.generic.ci.Container;
+import com.xebialabs.deployit.plugin.overthere.Host;
 
 @SuppressWarnings("serial")
-@Metadata(description = "An email server")
-public class MailServer extends Container {
-    private static final String HOST_ADDRESS_PROPERTY = "address";
+@Metadata(root = INFRASTRUCTURE, description = "An email server")
+public class MailServer extends Host {
+    private static final String ADDRESS_PROPERTY = "address";
     private static final String SMTP_PORT_PROPERTY = "smtpPort";
     private static final String SMTP_USERNAME_PROPERTY = "smtpUsername";
     private static final String SMTP_PASSWORD_PROPERTY = "smtpPassword";
     private static final String SMTP_TRANSPORT_PROPERTY = "smtpTransport";
     
     public Mailer getMailer() {
-        return new Mailer(getHost().<String>getProperty(HOST_ADDRESS_PROPERTY),
+        return new Mailer(this.<String>getProperty(ADDRESS_PROPERTY),
                 this.<Integer>getProperty(SMTP_PORT_PROPERTY),
                 this.<String>getProperty(SMTP_USERNAME_PROPERTY),
                 this.<String>getProperty(SMTP_PASSWORD_PROPERTY),
