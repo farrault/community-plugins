2f21ac3db2ce106be9359bc02c0e108861c7313e
Benoit Moussaud <bmoussaud@xebialabs.com>
2012-01-30 15:23:25 +0100

    RPM Plugin: define a generic Rpm Container allowing to execute a command before and after the deployment

--- rpm-plugin/src/test/java/com/xebialabs/deployit/plugin/rpm/TopologyFactory.java
+++ rpm-plugin/src/test/java/com/xebialabs/deployit/plugin/rpm/TopologyFactory.java
@@ -1,6 +1,7 @@
 package com.xebialabs.deployit.plugin.rpm;
 
 
+import com.xebialabs.deployit.plugin.generic.ci.Container;
 import com.xebialabs.deployit.plugin.overthere.Host;
 import com.xebialabs.deployit.test.support.TestUtils;
 import com.xebialabs.overthere.OperatingSystemFamily;
@@ -11,6 +12,7 @@ import static com.xebialabs.overthere.ssh.SshConnectionType.SFTP;
 
 public class TopologyFactory {
 	public static Host sshHost;
+	public static Container rpmContainer;
 
 	static {
 		initializeTopology();
@@ -24,7 +26,12 @@ public class TopologyFactory {
 		sshHost.setProperty("address", "ora-10g-express-unix");
 		sshHost.setProperty("username", "root");
 		sshHost.setProperty("password", "centos");
-		sshHost.setProperty("tmpDeleteOnDisconnect",false);
+		sshHost.setProperty("tmpDeleteOnDisconnect", false);
+
+		rpmContainer = TestUtils.newInstance("rpm.Container");
+		rpmContainer.setId(id("Infrastructure", "aHost", "aHostC"));
+		rpmContainer.setProperty("host", sshHost);
+
 
 	}
 
